1c74e679bd6c7bd0d0f8e9a50c7d4b639ee73b7b
Ali Alafandy <107328467+AliAlafandy@users.noreply.github.com>
2025-06-18 17:18:48 +0300

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -1,9 +1,5 @@
 package;
 
-#if android
-import android.content.Context;
-#end
-
 import debug.FPSCounter;
 
 import flixel.graphics.FlxGraphic;
@@ -69,7 +65,7 @@ class Main extends Sprite
 
 		// Credits to MAJigsaw77 (he's the og author for this code)
 		#if android
-		Sys.setCwd(Path.addTrailingSlash(Context.getExternalFilesDir()));
+		Sys.setCwd(Path.addTrailingSlash(AndroidContext.getExternalFilesDir()));
 		#elseif ios
 		Sys.setCwd(lime.system.System.applicationStorageDirectory);
 		#end
